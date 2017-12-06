fba986c4d197c125ea221c22532ac0207ca883ec
Sazzad Hossain Khan <itsazzad@gmail.com>
2017-12-06 19:31:22 +0600

    connect-signal initiate call

--- index.js
+++ index.js
@@ -112,35 +112,38 @@ MediaSession.prototype = extend(MediaSession.prototype, {
 
         this.pc.isInitiator = true;
         this.pc.offer(offerOptions, function (err, offer) {
-            console.error('START', offerOptions, err, offer);
             if (err) {
                 self._log('error', 'Could not create WebRTC offer', err);
                 return self.end('failed-application', true);
             }
 
-            // a workaround for missing a=sendonly
-            // https://code.google.com/p/webrtc/issues/detail?id=1553
-            if (offerOptions && offerOptions.mandatory) {
-                offer.jingle.contents.forEach(function (content) {
-                    var mediaType = content.application.media;
+            if (offer.jingle) {
+                // a workaround for missing a=sendonly
+                // https://code.google.com/p/webrtc/issues/detail?id=1553
+                if (offerOptions && offerOptions.mandatory) {
+                    offer.jingle.contents.forEach(function (content) {
+                        var mediaType = content.application.media;
 
-                    if (!content.description || content.application.applicationType !== 'rtp') {
-                        return;
-                    }
+                        if (!content.description || content.application.applicationType !== 'rtp') {
+                            return;
+                        }
 
-                    if (!offerOptions.mandatory.OfferToReceiveAudio && mediaType === 'audio') {
-                        content.senders = 'initiator';
-                    }
+                        if (!offerOptions.mandatory.OfferToReceiveAudio && mediaType === 'audio') {
+                            content.senders = 'initiator';
+                        }
 
-                    if (!offerOptions.mandatory.OfferToReceiveVideo && mediaType === 'video') {
-                        content.senders = 'initiator';
-                    }
-                });
+                        if (!offerOptions.mandatory.OfferToReceiveVideo && mediaType === 'video') {
+                            content.senders = 'initiator';
+                        }
+                    });
+                }
+
+                offer.jingle.contents.forEach(filterUnusedLabels);
+
+                self.send('session-initiate', offer.jingle);
+            } else {
+                self.send('session-initiate', offer.sdp);
             }
-
-            offer.jingle.contents.forEach(filterUnusedLabels);
-
-            self.send('session-initiate', offer.jingle);
 
             next();
         });
